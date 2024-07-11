0f6a41608cff90820097384c7f504dc099583999
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2024-07-11 13:16:12 +0300

    bot add buttons Выберите раздел

--- bot/index.js
+++ bot/index.js
@@ -1,8 +1,8 @@
 import TelegramBot from 'node-telegram-bot-api';
+
 const token = '6965532642:AAEGkS3VeQqHYKPueJ0V-xqo4TfPzdSWipU';
 
-// Создаем экземпляр бота
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, {polling: true});
 
 // Слушаем команду /start
 bot.onText(/\/start/, (msg) => {
@@ -13,7 +13,7 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(chatId, welcomeMessage, {
         reply_markup: {
             keyboard: [
-                [{ text: 'Старт' }]
+                [{text: 'Старт'}]
             ],
             resize_keyboard: true,
             one_time_keyboard: true
@@ -28,17 +28,20 @@ bot.on('message', (msg) => {
         // Отправляем приветственное видео и текст
         const videoPath = './assets/1th.mp4';
         const welcomeDescription = `
-            Добро пожаловать в нашего бота!
-            Этот бот может помочь вам с [описание функционала бота].
+*Супер-Бонусы лучшего казино Казахстана!*
+*Рейтинг популярных слотов*
+*Слоты с самыми большими выигрышами*
+*Победные схемы от наших подписчиков*
+*Вопрос-ответ и отзыв*
         `;
 
         // Отправляем видео
-        bot.sendVideo(chatId,  videoPath ).then(() => {
+        bot.sendVideo(chatId, videoPath).then(() => {
             // Отправляем текстовое сообщение после видео
             bot.sendMessage(chatId, welcomeDescription, {
                 reply_markup: {
                     keyboard: [
-                        [{ text: 'Начать использование' }]
+                        [{text: 'Начать использование'}]
                     ],
                     resize_keyboard: true,
                     one_time_keyboard: true
@@ -48,5 +51,51 @@ bot.on('message', (msg) => {
             console.error('Ошибка при отправке видео:', err);
             bot.sendMessage(chatId, 'Не удалось отправить видео. Попробуйте позже.');
         });
+    }
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    if (msg.text === 'Начать использование') {
+        // Отправляем новый раздел с кнопками через Inline Keyboard
+        const sectionMessage = 'Выберите раздел:';
+        bot.sendMessage(chatId, sectionMessage, {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'Бонусы и акции', callback_data: 'bonuses' }],
+                    [{ text: 'TOP популярных слотов', callback_data: 'top_slots' }],
+                    [{ text: 'TOP выигрышей', callback_data: 'top_wins' }],
+                    [{ text: 'Победные схемы', callback_data: 'winning_strategies' }],
+                    [{ text: 'Вопрос-ответ-отзывы', callback_data: 'qna_reviews' }]
+                ]
+            }
+        });
+    }
+});
+
+// Обработчик для Inline Keyboard
+bot.on('callback_query', (callbackQuery) => {
+    const chatId = callbackQuery.message.chat.id;
+    const data = callbackQuery.data;
+
+    // Обработка нажатия на кнопки Inline Keyboard
+    switch (data) {
+        case 'bonuses':
+            bot.sendMessage(chatId, 'Здесь будут бонусы и акции');
+            break;
+        case 'top_slots':
+            bot.sendMessage(chatId, 'Здесь будет список TOP популярных слотов');
+            break;
+        case 'top_wins':
+            bot.sendMessage(chatId, 'Здесь будет список TOP выигрышей');
+            break;
+        case 'winning_strategies':
+            bot.sendMessage(chatId, 'Здесь будут победные стратегии');
+            break;
+        case 'qna_reviews':
+            bot.sendMessage(chatId, 'Здесь будут вопросы-ответы и отзывы');
+            break;
+        default:
+            break;
     }
 });
