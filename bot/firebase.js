777e87f29909c8fdb12e2ea93a43b2258569d582
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2024-07-15 13:59:27 +0300

    refactoring keys

--- bot/firebase.js
+++ bot/firebase.js
@@ -6,11 +6,4 @@ export const firebaseConfig = {
     messagingSenderId: process.env.MESSAGESENDID,
     appId: process.env.APPID
 };
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCHzKeTsZ43c86z4y6cvbZDOtNxnl0CM7U",
-//     authDomain: "tgbot-cc51a.firebaseapp.com",
-//     projectId: "tgbot-cc51a",
-//     storageBucket: "tgbot-cc51a.appspot.com",
-//     messagingSenderId: "42637878178",
-//     appId: "1:42637878178:web:129d927716a8ee291288fb"
-// };
+
