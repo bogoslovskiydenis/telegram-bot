acce260c14aef4f769f147311b13417116d31c6e
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2024-07-17 16:11:01 +0300

    add firebse tokens

---
+++ admin-panel/src/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCHzKeTsZ43c86z4y6cvbZDOtNxnl0CM7U",
+    authDomain: "tgbot-cc51a.firebaseapp.com",
+    projectId: "tgbot-cc51a",
+    storageBucket: "tgbot-cc51a.appspot.com",
+    messagingSenderId: "42637878178",
+    appId: "1:42637878178:web:129d927716a8ee291288fb"
+};
+
